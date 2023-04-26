893234779a089f2197986d6a4cec75f900a8f824
Elismar Alves <elismar.eaf@gmail.com>
2023-04-26 00:57:49 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -8,21 +7,58 @@ public class Main {
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("Curso de Java da DIO");
-        curso1.setCagarHoraria(8);
+        curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso JS");
         curso2.setDescricao("Curso de JS da DIO");
-        curso2.setCagarHoraria(4);
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Mentoria do Curso de Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer (DIO)");
+        bootcamp.setDescricao("Bootcamp Java Developer (DIO) - Oferecimento Banco Pan");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos por Camila: " + devCamila.getConteudosInscritos());
+
+        devCamila.progredir();
+        devCamila.progredir();
+
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos por Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos por João: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+
+        System.out.println("--------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos por João: " + devJoao.getConteudosInscritos());
+
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos por João: " + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos por João: " + devJoao.getConteudosConcluidos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
+
 
     }
 }
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,43 +1,38 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
-    private int cagarHoraria;
+/*public class Curso extends Conteudo{
+
+}*/
+
+
+public class Curso extends Conteudo {
+
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
 
     public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
+    public int getcargaHoraria() {
+        return cargaHoraria;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public int getCagarHoraria() {
-        return cagarHoraria;
-    }
-
-    public void setCagarHoraria(int cagarHoraria) {
-        this.cagarHoraria = cagarHoraria;
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
     }
 
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", cagarHoraria=" + cagarHoraria +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cagarHoraria=" + cargaHoraria +
                 '}';
     }
+
 }
